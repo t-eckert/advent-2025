d5af1b7ef56c19329d306b950b1da790b0eadd8b
Thomas Eckert <thomas.james.eckert@gmail.com>
2025-12-01 14:39:56 -0500

    Day 1 cleanup

--- src/day_01.rs
+++ src/day_01.rs
@@ -1,6 +1,6 @@
 use crate::{puzzle::Parts, timer::format_duration};
 use anyhow::anyhow;
-use std::{any, error::Error, fs::read_to_string, time::Instant};
+use std::{fs::read_to_string, time::Instant};
 
 const NAME: &str = "Day 1";
 const INPUT_FILE: &str = "inputs/day_01.txt";
@@ -8,31 +8,48 @@ const INPUT_FILE: &str = "inputs/day_01.txt";
 pub fn run(parts: Parts) {
     log::debug!("{}", NAME);
 
-    let input = read_to_string(INPUT_FILE)
-        .unwrap_or_else(|e| panic!("Failed to read {}: {}", INPUT_FILE, e));
+    let turns = parse_input(
+        &read_to_string(INPUT_FILE)
+            .unwrap_or_else(|e| panic!("Failed to read {}: {}", INPUT_FILE, e)),
+    )
+    .expect("Failed to parse");
 
     let start = Instant::now();
     match parts {
         Parts::Both => {
-            println!("{}", part_1(&input).unwrap());
-            println!("{}", part_2(&input));
+            println!("{}", part_1(&turns));
+            println!("{}", part_2(&turns));
         }
         Parts::First => {
-            println!("{}", part_1(&input).unwrap());
+            println!("{}", part_1(&turns));
         }
         Parts::Second => {
-            println!("{}", part_2(&input));
+            println!("{}", part_2(&turns));
         }
     };
     let duration = start.elapsed();
     log::debug!("Duration {}", format_duration(duration));
 }
 
-fn part_1(input: &str) -> Result<i32, anyhow::Error> {
-    log::debug!("{}", input);
+fn parse_input(input: &str) -> Result<Vec<i32>, anyhow::Error> {
+    input.lines().map(parse_line).collect()
+}
 
-    let turns = parse_input(input)?;
+fn parse_line(line: &str) -> Result<i32, anyhow::Error> {
+    let (dir, count) = line.split_at(1);
 
+    let sign = if dir == "L" {
+        -1
+    } else if dir == "R" {
+        1
+    } else {
+        return Err(anyhow!("Invalid direction"));
+    };
+
+    Ok(sign * count.parse::<i32>()?)
+}
+
+fn part_1(turns: &Vec<i32>) -> i32 {
     let max_idx = 99;
     let min_idx = 0;
     let n_indices = 100; // Because we include the 0 as an option
@@ -41,19 +58,16 @@ fn part_1(input: &str) -> Result<i32, anyhow::Error> {
     let mut n_zeros = 0;
     for turn in turns {
         // Apply, then normalize
-        idx = match turn.dir {
-            Dir::L => idx - turn.count,
-            Dir::R => idx + turn.count,
-        };
+        idx += turn;
 
         // Handle negative rollover.
         while idx < min_idx {
-            idx = idx + n_indices;
+            idx += n_indices;
         }
 
         // Handle positive rollover
         while idx > max_idx {
-            idx = idx - n_indices;
+            idx -= n_indices;
         }
 
         if idx == 0 {
@@ -61,14 +75,10 @@ fn part_1(input: &str) -> Result<i32, anyhow::Error> {
         }
     }
 
-    Ok(n_zeros)
+    n_zeros
 }
 
-fn part_2(input: &str) -> i32 {
-    log::debug!("{}", input);
-
-    let turns = parse_input(input).unwrap();
-
+fn part_2(turns: &Vec<i32>) -> i32 {
     let max_idx = 99;
     let min_idx = 0;
     let n_indices = 100; // Because we include the 0 as an option
@@ -76,62 +86,23 @@ fn part_2(input: &str) -> i32 {
     let mut idx = 50;
     let mut n_zeros = 0;
     for turn in turns {
-        dbg!(idx);
-        dbg!(&turn);
         // Apply, then normalize
-        idx = match turn.dir {
-            Dir::L => idx - turn.count,
-            Dir::R => idx + turn.count,
-        };
+        idx += turn;
 
         // Handle negative rollover.
         while idx < min_idx {
-            dbg!("Handling negative rollover");
             n_zeros += 1;
-            idx = idx + n_indices;
+            idx += n_indices;
         }
 
         // Handle positive rollover
         while idx > max_idx {
-            dbg!("Handling positive rollover");
             n_zeros += 1;
-            idx = idx - n_indices;
+            idx -= n_indices;
         }
-
-        dbg!(idx);
-        dbg!(n_zeros);
     }
 
     n_zeros
-}
-
-#[derive(Debug)]
-enum Dir {
-    L,
-    R,
-}
-
-#[derive(Debug)]
-struct Turn {
-    dir: Dir,
-    count: i32,
-}
-
-fn parse_input(input: &str) -> Result<Vec<Turn>, anyhow::Error> {
-    input.lines().map(parse_line).collect()
-}
-
-fn parse_line(line: &str) -> Result<Turn, anyhow::Error> {
-    let (dir, count) = line.split_at(1);
-
-    Ok(Turn {
-        dir: match dir {
-            "L" => Dir::L,
-            "R" => Dir::R,
-            _ => return Err(anyhow!("Invalid direction")),
-        },
-        count: count.parse()?,
-    })
 }
 
 #[cfg(test)]
@@ -151,13 +122,15 @@ L82";
 
     #[test]
     fn test_part_1() {
-        let result = part_1(&TEST_INPUT).unwrap();
+        let turns = parse_input(&TEST_INPUT).unwrap();
+        let result = part_1(&turns);
         assert_eq!(result, 3);
     }
 
     #[test]
     fn test_part_2() {
-        let result = part_2(&TEST_INPUT);
+        let turns = parse_input(&TEST_INPUT).unwrap();
+        let result = part_2(&turns);
         assert_eq!(result, 6);
     }
 }
